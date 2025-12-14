ecde37931538ecb84c89d945204ef9eef7bb6163
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-12-15 09:42:57 +1100

    setup basic server.js file

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+// Load environment variables
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
