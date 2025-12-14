cef95862bc237e7b63c6904cefefb88b05d499e8
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-12-15 10:57:03 +1100

    configured cors, morgan and created some test routes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
 
 // Load environment variables
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+// Constants
+const IP = process.env.IP
+const PORT = process.env.PORT;
+const GREEN = '\x1b[32m';
+const WHITE = '\x1b[37m';
+
+// Middleware
+// TODO: Allow all origins for now
+app.use(cors());
+app.use(morgan('dev'));
+
+// app.get('/', (req, res) => {
+//     res.json({message: 'Hello World!'});
+// });
+
+// app.get('/:name', (req, res) => {
+//     res.json({message: `Hello ${req.params.name}!`});
+// });
+
+app.listen(PORT, IP, () => {
+    console.log(GREEN + `Server started at http://${IP}:${PORT}` + WHITE);
+});
