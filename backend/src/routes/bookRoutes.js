d4232061e4ab4a10c0437b3946732bee05023777
Kevin <185861753+Scrasms@users.noreply.github.com>
2026-01-27 09:43:39 +1100

    commented routes where further explanation  was needed

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -12,18 +12,24 @@ import {
 
 const bookRouter = Router();
 
+// Get information about all published books
 bookRouter.get("/", bookInfoAll);
 
 bookRouter.post("/", isAuth, bookCreate);
 
+// Get information about a specific book
+// Book must be published to be found, unless the user owns the book
 bookRouter.get("/:bid", bookInfo);
 
+// Updates a book's title, blurb, book cover and 'published' status
 bookRouter.put("/:bid", isAuth, bookUpdate);
 
 bookRouter.delete("/:bid", isAuth, bookDelete);
 
+// Adds a new tag to a book owned by the user
 bookRouter.post("/:bid/tag", isAuth, bookTag);
 
+// Deletes a specific tag from a book owned by the user
 bookRouter.delete("/:bid/tag", isAuth, bookUntag);
 
 export default bookRouter;
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -21,12 +21,16 @@ userRouter.post("/logout", isAuth, userLogout);
 
 userRouter.post("/delete", isAuth, userDelete);
 
+// Update user's password
 userRouter.post("/password", isAuth, userPassword);
 
+// Update user's profile picture
 userRouter.put("/profile", isAuth, userProfile);
 
+// Test route that only works if user is authenticated
 userRouter.get("/", isAuth, userTest);
 
+// Get public information about the given user
 userRouter.get("/:username", userInfo);
 
 export default userRouter;
