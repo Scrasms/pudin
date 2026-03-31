642534218646d0b7cf8721bd3770833c2ffbc643
Kevin <185861753+Scrasms@users.noreply.github.com>
2026-03-31 12:49:47 +1100

    removed MUI

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,13 +1,7 @@
-import { Box, Container, Typography } from '@mui/material';
-
 function Landing() {
   return (
     <>
-      <Container>
-        <Box sx={{ textAlign: 'center', color: 'secondary.dark' }}>
-          <Typography variant="h1">Hello!</Typography>
-        </Box>
-      </Container>
+      <h1>Hello!</h1>
     </>
   );
 }
