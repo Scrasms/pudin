c5762f692ea48e65e71d7ff403eb98127244763b
Kevin <185861753+Scrasms@users.noreply.github.com>
2026-03-31 13:11:47 +1100

    Revert "removed MUI"
    This reverts commit 642534218646d0b7cf8721bd3770833c2ffbc643.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App.tsx';
 import './index.css';
 
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#f9f7c3',
+    },
+    primary: {
+      main: '#f9f7c3',
+    },
+    secondary: {
+      main: '#a97d70',
+      dark: '#83473a',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Inter',
+      'Roboto',
+      'Arial',
+      'Helvetica',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
